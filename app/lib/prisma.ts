d1250a2d79d56a0eca4b7b519d70efaeab356d45
Thabang Joy Moruntshigit config --global user.email 318551018+thabangjoymoruntshi@users.noreply.github.com <318551018+thabangjoymoruntshi@users.noreply.github.com>
2026-09-16 15:45:21 +0200

    Fix Prisma client deployment

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,7 +1,6 @@
-
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const connectionString = process.env.DATABASE_URL;
 
